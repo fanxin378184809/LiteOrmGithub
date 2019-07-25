cdd6cda052111ca733139697680c0fbd98b2f7aa
范鑫 <378184809@qq.com>
2019-07-25 17:47:37 +0800

    测试更新

--- app/src/main/java/com/github/fanxin/liteorm/MainActivity.java
+++ app/src/main/java/com/github/fanxin/liteorm/MainActivity.java
@@ -11,10 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        LiteOrmUtil.init(getApplication());
-//        LiteOrmUtil.setSdDbPath(Environment.getExternalStorageDirectory().getPath() + "/likun/db");
-//        LiteOrmUtil.get().save(new CMyData(22,"asdasasd",true));
-
         LiteOrmUtil.init(getApplication());
         LiteOrmUtil.setSdDbPath(Environment.getExternalStorageDirectory().getPath()+"/fanxin/db");
         LiteOrmUtil.get().save(new CMyData(22,"fanxin",true));
