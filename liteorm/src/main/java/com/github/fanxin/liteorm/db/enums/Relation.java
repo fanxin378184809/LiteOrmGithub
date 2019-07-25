2bdd8e24fec97702f1a8574eb5bd4150f4bea4ec
范鑫 <378184809@qq.com>
2019-07-25 16:39:53 +0800

    第一次提交LiteOrm

---
+++ liteorm/src/main/java/com/github/fanxin/liteorm/db/enums/Relation.java
@@ -0,0 +1,8 @@
+package com.github.fanxin.liteorm.db.enums;
+
+public enum Relation {
+    ManyToMany,
+    OneToMany,
+    ManyToOne,
+    OneToOne
+}
---
+++ liteorm/src/main/java/com/github/fanxin/liteorm/kvdb/FileDataCahe.java
@@ -0,0 +1,5 @@
+package com.github.fanxin.liteorm.kvdb;
+
+public interface FileDataCahe {
+	
+}
