047c51622bd43a1c798a92c366f468cb9b47e9f8
范鑫 <378184809@qq.com>
2019-07-26 13:11:44 +0800

    修改工程gradle版本号从3.2.1 修改成 3.4.2

--- app/src/main/java/com/fanxin/test/MainActivity.java
+++ app/src/main/java/com/fanxin/test/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.github.fanxin.liteorm.LiteOrmUtil;
-import com.github.fanxin.liteormtest.R;
 
 public class MainActivity extends AppCompatActivity {
 
